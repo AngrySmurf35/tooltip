53f140e89b30b85ed05a3bf69136462d40a51325
razbogdan <rbogdan@temenos.com>
2016-03-25 18:19:40 +0200

    tooltips colide v0.1

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -3,28 +3,33 @@ var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
 
-$('body').append('<div><h1 class="test" style="display: inline-block">Test</h1></div>');
-$('body').append('<div><h1 class="test1" style="display: inline-block">Test1</h1></div>');
-$('body').append('<div><h1 class="test2" style="display: inline-block">Test2</h1></div>');
+$('body').append('<div style="display: inline-block"><h1 class="test">Test</h1></div>');
+$('body').append('<div style="display: inline-block;margin-right:100px"><h1 class="test1">Test1</h1></div>');
+$('body').append('<div style="display: inline-block"><h1 class="test2">Test2</h1></div>');
 
 var home = new Home({
   "element": $('.test'),
   "message": "Every day I'm hovering",
-  "place": 'right'
+  "place": 'left',
+  "tooltipWidth": 150
 });
 
 var home1 = new Home({
   "element": $('.test1'),
   "message": "Every day I'm hovering",
-  "place": 'right'
+  "place": 'left',
+  "tooltipWidth": 150
 });
 
 var home2 = new Home({
   "element": $('.test2'),
   "message": "Every day I'm hovering",
-  "place": 'right'
+  "place": 'left',
+  "tooltipWidth": 150
 });
 
+
+// after render
 home.display();
 home1.display();
 home2.display();
--- app/scripts/home.js
+++ app/scripts/home.js
@@ -24,10 +24,9 @@ module.exports = Backbone.View.extend({
         */
         initialize: function(options) {
 
-            this.settings = _.extend( {}, this.settings, options );
+          this.settings = _.extend( {}, this.settings, options );
 
-            this.class = "";
-            this._addClass();
+          this.caret_class = "";
         },
 
         /*
@@ -35,118 +34,127 @@ module.exports = Backbone.View.extend({
         */
 
         _addClass: function() {
-            switch(this.settings.place) {
-                case 'right':
-                    this.caret_class = "caret-toggler-left";
-                    break;
-                case 'left':
-                    this.caret_class = "caret-toggler-right";
-                    break;
-                case 'top':
-                    this.caret_class = "caret-toggler-top";
-                    break;
-                case 'bottom':
-                    this.caret_class = "caret-toggler-bottom";
-                    break;
-                default:
-                    this.caret_class = "";
-                    break;
-            }
+          if ($(this.settings.element).offset().left < this.settings.tooltipWidth) {
+            this.settings.place = "right";
+          }
+
+          switch(this.settings.place) {
+            case 'right':
+              this.caret_class = "caret-toggler-left";
+              break;
+            case 'left':
+              this.caret_class = "caret-toggler-right";
+              break;
+            case 'top':
+              this.caret_class = "caret-toggler-top";
+              break;
+            case 'bottom':
+              this.caret_class = "caret-toggler-bottom";
+              break;
+            default:
+              this.caret_class = "";
+              break;
+          }
 
         },
 
         /*
         * @function aligning the tooltip relative to the element
         */
-        align: function() {
+        _align: function() {
+          var caretSize = 10;
+          var posValues = {
+              verticalTop: $(this.settings.element).offset().top - this.$el.innerHeight() - caretSize,
+              verticalMiddle: $(this.settings.element).offset().top - this.$el.innerHeight()/2 + $(this.settings.element).innerHeight()/2,
+              verticalBottom: $(this.settings.element).offset().top + $(this.settings.element).innerHeight() + caretSize,
 
+              horizontalLeft: $(this.settings.element).offset().left - this.$el.innerWidth() - caretSize,
+              horizontalRight: $(this.settings.element).offset().left + $(this.settings.element).innerWidth() + caretSize,
+              horizontalMiddle: $(this.settings.element).offset().left - $(this.settings.element).innerWidth()/2 + caretSize,
 
-            var caretSize = 10;
-            var posValues = {
-                verticalTop: $(this.settings.element).offset().top - this.$el.innerHeight() - caretSize,
-                verticalMiddle: $(this.settings.element).offset().top - this.$el.innerHeight()/2 + $(this.settings.element).innerHeight()/2,
-                verticalBottom: $(this.settings.element).offset().top + $(this.settings.element).innerHeight() + caretSize,
+              caretHorizontalLeft: -caretSize,
+              caretHorizontalRight: this.$el.innerWidth(),
+              caretHorizontalMiddle: this.$el.innerHeight() + caretSize,
 
-                horizontalLeft: $(this.settings.element).offset().left - this.$el.innerWidth() - caretSize,
-                horizontalRight: $(this.settings.element).offset().left + $(this.settings.element).innerWidth() + caretSize,
-                horizontalMiddle: $(this.settings.element).offset().left - $(this.settings.element).innerWidth() + caretSize,
+              caretVerticalTop: -caretSize,
+              caretVerticalMiddle: this.$el.innerHeight()/2 - caretSize,
+              caretVerticalBottom: this.$el.innerHeight()
+          };
+          var position;
+          switch(this.settings.place) {
+              case 'right':
+                  position = {
+                      left: posValues.horizontalRight,
+                      top: posValues.verticalMiddle,
+                      caret_top: posValues.caretVerticalMiddle,
+                      caret_left: posValues.caretHorizontalLeft
+                  };
+                  break;
+              case 'left':
+                  position = {
+                      left: posValues.horizontalLeft,
+                      top: posValues.verticalMiddle,
+                      caret_top: posValues.caretVerticalMiddle,
+                      caret_left: posValues.caretHorizontalRight
+                  };
+                  break;
+              case 'top':
+                  position = {
+                      left: posValues.horizontalMiddle,
+                      top: posValues.verticalTop,
+                      caret_top: posValues.caretVerticalBottom,
+                      caret_left: posValues.caretHorizontalMiddle
+                  };
+                  break;
+              case 'bottom':
+                  position = {
+                      left: posValues.horizontalMiddle,
+                      top: posValues.verticalBottom,
+                      caret_top: posValues.caretVerticalTop,
+                      caret_left: posValues.caretHorizontalMiddle
+                  };
+                  break;
+              default:
+                  position = {
+                      left: $(this.settings.element).position().left,
+                      top: $(this.settings.element).position().top,
+                      caret_pos: 0
+                  };
+                  break;
+          }
+          this.$el.offset({top:position.top,left:position.left});
 
-                caretHorizontalLeft: -caretSize,
-                caretHorizontalRight: this.$el.innerWidth(),
-                caretHorizontalMiddle: this.$el.innerHeight() + caretSize,
+          if (position && position.caret_top)
+              this.$el.find('.caret-toggler').css('top', position.caret_top + 'px');
 
-                caretVerticalTop: -caretSize,
-                caretVerticalMiddle: this.$el.innerHeight()/2 - caretSize,
-                caretVerticalBottom: this.$el.innerHeight()
-            };
-            var position;
-            switch(this.settings.place) {
-                case 'right':
-                    position = {
-                        left: posValues.horizontalRight,
-                        top: posValues.verticalMiddle,
-                        caret_top: posValues.caretVerticalMiddle,
-                        caret_left: posValues.caretHorizontalLeft
-                    };
-                    break;
-                case 'left':
-                    position = {
-                        left: posValues.horizontalLeft,
-                        top: posValues.verticalMiddle,
-                        caret_top: posValues.caretVerticalMiddle,
-                        caret_left: posValues.caretHorizontalRight
-                    };
-                    break;
-                case 'top':
-                    position = {
-                        left: posValues.horizontalMiddle,
-                        top: posValues.verticalTop,
-                        caret_top: posValues.caretVerticalBottom,
-                        caret_left: posValues.caretHorizontalMiddle
-                    };
-                    break;
-                case 'bottom':
-                    position = {
-                        left: posValues.horizontalMiddle,
-                        top: posValues.verticalBottom,
-                        caret_top: posValues.caretVerticalTop,
-                        caret_left: posValues.caretHorizontalMiddle
-                    };
-                    break;
-                default:
-                    position = {
-                        left: $(this.settings.element).position().left,
-                        top: $(this.settings.element).position().top,
-                        caret_pos: 0
-                    };
-                    break;
-            }
-            this.$el.offset({top:position.top,left:position.left});
+          if (position && position.caret_left)
+              this.$el.find('.caret-toggler').css('left', position.caret_left + 'px');
 
-            if (position && position.caret_top)
-                this.$el.find('.caret-toggler').css('top', position.caret_top + 'px');
+      },
 
-            if (position && position.caret_left)
-                this.$el.find('.caret-toggler').css('left', position.caret_left + 'px');
+      /*
+      * @function call this after render
+      */
+      display: function() {
+        var _this = this;
+        $(this.settings.element).on('mouseenter', function() {
+          $('body').append(_this.render().el);
+          _this._align();
+        });
 
-        },
+        $(this.settings.element).on('mouseout', function() {
+          _this.render().el.remove();
+        });
 
-        display: function() {
-          var _this = this;
-          $(this.settings.element).on('mouseenter', function() {
-            $('body').append(_this.render().el);
-            _this.align();
-          });
+      },
 
-          $(this.settings.element).on('mouseout', function() {
-            _this.render().el.remove();
-          });
+      render: function() {
 
-        },
+          // before render
+          this._addClass();
 
-        render: function() {
-            this.$el.html(this.template({ message: this.settings.message, caret_class: this.caret_class }));
-            return this;
-        }
+          this.$el.html(this.template({ message: this.settings.message, caret_class: this.caret_class }));
+          return this;
+      }
 
     });
